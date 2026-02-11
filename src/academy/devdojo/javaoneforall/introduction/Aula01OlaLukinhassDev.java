d05d4289752aa186fd23450e80c620392b1f49e6
Lukinhassdev <lucassilvamagalhaes352@gmail.com>
2026-02-11 12:56:19 -0300

    feat: setup inicial e aulas 01 e 02 da maratona

---
+++ src/academy/devdojo/javaoneforall/introduction/Aula01OlaLukinhassDev.java
@@ -0,0 +1,7 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Aula01OlaLukinhassDev {
+    public static void main(String []args) {
+        System.out.println("Olá lukinhas dev, seja bem vindo ao java");
+    }
+}
