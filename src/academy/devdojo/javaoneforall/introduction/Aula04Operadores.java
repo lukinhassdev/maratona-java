b443e39bc80678e49417e42f044079fdd1a02453
Lukinhassdev <lucassilvamagalhaes352@gmail.com>
2026-02-12 14:25:29 -0300

    Ajustando aula 04

--- src/academy/devdojo/javaoneforall/introduction/Aula04Operadores.java
+++ src/academy/devdojo/javaoneforall/introduction/Aula04Operadores.java
@@ -9,7 +9,6 @@ public class Aula04Operadores {
         int a = 15;
         int b = 5;
         int resultadoSoma = a + b;
-
         System.out.println("--------------------------------");
         System.out.println("VALOR DE SOMA");
         System.out.println("--------------------------------");
@@ -17,7 +16,6 @@ public class Aula04Operadores {
         System.out.println("o valor de a é :"+ a);
         System.out.println("o valor de b é :"+ b);
         System.out.println("a soma de a + b :"+ resultadoSoma);
-
         //Quando trablhamos com números onde realizamos uma conta que retorna um resultado com casas decimais o correto é trabalhar com variáveis do tipo double ou float, porque se o tipo da variável for inteiro ele retorna um valor inteiro
 
         System.out.println("--------------------------------");
@@ -30,7 +28,6 @@ public class Aula04Operadores {
         System.out.println("o valor de numero01 é :"+ numero01);
         System.out.println("o valor de numero02 é :"+ numero02);
         System.out.println("veja o resultado do numero01 dividido pelo numero02 :"+resultadoConta);
-
         //Nesse segundo caso abaixo, o valor do resultado dessa divisão vai retornar um número inteiro: 3
 
         System.out.println("--------------------------------");
@@ -42,6 +39,20 @@ public class Aula04Operadores {
         int resultadoDivisao = x / y;
         System.out.println("o valor de x:"+ x);
         System.out.println("o valor de y:"+ y);
-        System.out.println("resultado do valor de x dividido pelo valor de y:"+ resultadoDivisao);
+        System.out.println("resultado da divisão:"+ resultadoDivisao);
+        System.out.println();
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezDifenteDez = 10 != 10;
+
+        //Nesse caso abaixo retornará um true (Verdadeiro) por que estamos comparando números, se compararmos a uma string irá acontecer um erro de compilação.
+        boolean isDezIgualDez = 10 == 10.0;
+        System.out.println("isDezMaiorQueVinte :" + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte :" + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte :" + isDezIgualVinte);
+        System.out.println("isDezDifenteDez :" + isDezDifenteDez);
+        System.out.println("isDezIgualDez :" + isDezIgualDez);
     }
 }
