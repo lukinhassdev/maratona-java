08dda92b7d310857ce578584ea91225aec880905
Lukinhassdev <lucassilvamagalhaes352@gmail.com>
2026-04-20 04:52:30 -0300

    utilizando constantes para o código ficar mais oorganizado. day 20-04-26, 4:52 am

--- maratona-java/src/academy/devdojo/javaoneforall/introduction/Aula05EstruturasCondicionais04.java
+++ maratona-java/src/academy/devdojo/javaoneforall/introduction/Aula05EstruturasCondicionais04.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 public class Aula05EstruturasCondicionais04 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
+        double primeiraFaixa = 7.5 / 100;
+        double segundaFaixa = 9.0 / 100;
+        double terceiraFaixa = 12.0 / 100;
+        double quartaFaixa = 14.0 / 100;
+        double valorImposto;
 
         System.out.println("DIGITE QUAL O VALOR DO SEU SALÁRIO ANUAL:");
         double salarioAnual = ler.nextDouble();
-        double valorImposto;
         if (salarioAnual <= 1412) {
-            valorImposto = salarioAnual * (7.5 / 100);
+            valorImposto = salarioAnual * primeiraFaixa;
         } else if (salarioAnual <= 2666.98) {
-            valorImposto = salarioAnual * (9.0 / 100);
+            valorImposto = salarioAnual * segundaFaixa;
         } else if (salarioAnual <= 4000.03) {
-            valorImposto = salarioAnual * (12.0 / 100);
+            valorImposto = salarioAnual * terceiraFaixa;
         } else {
-            valorImposto = salarioAnual * (14.0 / 100);
+            valorImposto = salarioAnual * quartaFaixa;
         }
         System.out.println("O valor do imposto a ser pago é: R$ " + valorImposto);
     }
